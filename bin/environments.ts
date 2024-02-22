a5aada6b961106678f1ef41ad88d815790356279
AlexisToribio <alexistoba21@hotmail.com>
2024-02-22 08:30:59 -0500

    feat: setup project

---
+++ bin/environments.ts
@@ -0,0 +1,29 @@
+export const dev = {
+	region: 'us-east-2',
+	project: 'hrmgo',
+	environment: 'dev',
+	ownerAccount: 'mtoribio',
+	infraRepo: 'HRMGO_INFRA',
+	infraBranch: 'development',
+	// cicdRepo: 'HRMGO_PIPELINE',
+	// cicdBranch: 'development',
+	cicdRepo: 'pipelinetest',
+	cicdBranch: 'main',
+	appRepo: 'NewHRMv2.0',
+	appBranch: 'dev_morrison',
+	accountId: '364964202465',
+};
+
+export const prod = {
+	region: 'us-east-2',
+	project: 'hrmgo',
+	environment: 'prod',
+	ownerAccount: '',
+	infraRepo: 'HRMGO_INFRA',
+	infraBranch: 'main',
+	cicdRepo: 'HRMGO_PIPELINE',
+	cicdBranch: 'main',
+	appRepo: 'NewHRMv2.0',
+	appBranch: 'main',
+	accountId: '364964202465',
+};

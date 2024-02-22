913ca262152d68572f12f8a597e01270176bdc02
AlexisToribio <alexistoba21@hotmail.com>
2024-02-22 08:31:14 -0500

    feat: add cicd stack

--- bin/foundational.ts
+++ bin/foundational.ts
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { FoundationalStack } from '../lib/foundational-stack';
+import { createName } from '../utils/createName';
+import { CiCdPipelineStack } from '../lib/pipeline-cicd-stack';
+import { dev as env } from '../bin/environments';
 
 const app = new cdk.App();
-new FoundationalStack(app, 'FoundationalStack');
+
+const { project } = env;
+
+const cicdPipelineStack = new CiCdPipelineStack(app, createName('stack', 'cicd-pipeline'), { env });
+
+cdk.Tags.of(cicdPipelineStack).add('proyecto', project);
